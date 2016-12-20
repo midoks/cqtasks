188aeceeb050cd3a4d7b4c1baea69f7d8a4c01b6
midoks <midoks@163.com>
2016-12-20 20:10:20 +0800

    update

--- src/app/cqtasks.c
+++ src/app/cqtasks.c
@@ -2,6 +2,12 @@
 
 int main()
 {
+	cq_projects t;
+
+	int i = sizeof(t);
+
+	printf("%d\n", i);
+
 	printf("%s\n", "hello,world!!");
 	return 0;
 }
--- src/app/cqtasks.h
+++ src/app/cqtasks.h
@@ -1,2 +1,46 @@
 #include <stdio.h>
 
+typedef struct _cq_projects 	cq_projects;
+typedef struct _cq_crons    	   cq_crons;
+typedef struct _cq_cron_cmd 	cq_cron_cmd;
+
+typedef struct _cq_queues   	  cq_queues;
+typedef struct _cq_queue_cmd   cq_queue_cmd;
+
+
+struct _cq_projects
+{
+	char 		  *name;
+	cq_crons 	 *crons;
+	cq_queues 	*queues;
+};
+
+
+struct _cq_crons
+{
+	char  		*name;
+	cq_cron_cmd  *cmd;
+	cq_crons    *prev;
+	cq_crons    *next;
+};
+
+struct _cq_cron_cmd
+{
+	int min; 	//0-59
+	int hour; 	//0-23
+	int day;	//1-31
+	int month;	//1-12
+	int week;	//0-6
+};
+
+struct _cq_queues {
+	char 		 *name;
+	cq_queue_cmd  *cmd;
+	cq_queues    *prev;
+	cq_queues    *next;
+};
+
+struct _cq_queue_cmd {
+	char *name;
+	
+};
